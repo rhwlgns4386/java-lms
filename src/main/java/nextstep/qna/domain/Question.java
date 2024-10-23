b6d1b601b5207eaba9d80fe07b58586e76de8ba5
pum005 <pum005@naver.com>
2024-10-23 20:43:09 +0900

    refactor : question 인스턴스 변수 줄이기

--- src/main/java/nextstep/qna/domain/Question.java
+++ src/main/java/nextstep/qna/domain/Question.java
@@ -5,71 +5,45 @@ import nextstep.users.domain.NsUser;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Question {
-    private Long id;
 
-    private String title;
 
-    private String contents;
-
-    private NsUser writer;
-
-    private List<Answer> answers = new ArrayList<>();
-
+    private final BaseEntity baseEntity;
+    private final QuestionComments questionComments;
+    private final Answers answers;
     private boolean deleted = false;
 
-    private LocalDateTime createdDate = LocalDateTime.now();
-
-    private LocalDateTime updatedDate;
-
-    public Question() {
-    }
 
     public Question(NsUser writer, String title, String contents) {
         this(0L, writer, title, contents);
     }
 
     public Question(Long id, NsUser writer, String title, String contents) {
-        this.id = id;
-        this.writer = writer;
-        this.title = title;
-        this.contents = contents;
+        this(new BaseEntity(id), new QuestionComments(title, contents, writer), new Answers());
+    }
+
+    public Question(BaseEntity baseEntity, QuestionComments questionComments, Answers answers) {
+        this.baseEntity = baseEntity;
+        this.questionComments = questionComments;
+        this.answers = answers;
     }
 
     public Long getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public Question setTitle(String title) {
-        this.title = title;
-        return this;
-    }
-
-    public String getContents() {
-        return contents;
-    }
-
-    public Question setContents(String contents) {
-        this.contents = contents;
-        return this;
+        return this.baseEntity.getId();
     }
 
     public NsUser getWriter() {
-        return writer;
+        return this.questionComments.getWriter();
     }
 
     public void addAnswer(Answer answer) {
-        answer.toQuestion(this);
         answers.add(answer);
     }
 
     public boolean isOwner(NsUser loginUser) {
-        return writer.equals(loginUser);
+        return this.questionComments.getWriter().equals(loginUser);
     }
 
     public Question setDeleted(boolean deleted) {
@@ -82,11 +56,19 @@ public class Question {
     }
 
     public List<Answer> getAnswers() {
-        return answers;
+        return this.answers.getAnswers();
     }
 
     @Override
-    public String toString() {
-        return "Question [id=" + getId() + ", title=" + title + ", contents=" + contents + ", writer=" + writer + "]";
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Question)) return false;
+        Question question = (Question) o;
+        return deleted == question.deleted && Objects.equals(baseEntity, question.baseEntity) && Objects.equals(questionComments, question.questionComments) && Objects.equals(answers, question.answers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(baseEntity, questionComments, answers, deleted);
     }
 }
--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -1,8 +1,23 @@
 package nextstep.qna.domain;
 
+import nextstep.users.domain.NsUser;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class QuestionTest {
     public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
     public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+
+    @Test
+    void create() {
+        BaseEntity baseEntity = new BaseEntity(1L, LocalDateTime.now(), LocalDateTime.now());
+        QuestionComments questionComments = new QuestionComments("title", "comments", new NsUser());
+        Answers answers = new Answers(List.of());
+        assertThat(new Question(baseEntity, questionComments, answers)).isEqualTo(new Question(baseEntity, questionComments, answers));
+    }
 }
