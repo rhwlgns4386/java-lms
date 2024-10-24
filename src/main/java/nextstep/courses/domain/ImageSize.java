7d5d51f45c891ba9b6afd5eaf28fc3914986f3a7
heechul <khc41@lgensol.com>
2024-10-24 16:25:44 +0900

    feat: ImageSize validation 하는 클래스 구현

---
+++ src/main/java/nextstep/courses/domain/ImageSize.java
@@ -0,0 +1,18 @@
+package nextstep.courses.domain;
+
+public class ImageSize {
+    private final int witdh;
+    private final int height;
+
+    public ImageSize(int width, int height) {
+        validateImageSize(width, height);
+        this.witdh = width;
+        this.height = height;
+    }
+
+    private static void validateImageSize(int width, int height) {
+        if (width < 300 || height < 200) {
+            throw new IllegalArgumentException("이미지의 규격이 맞지 않습니다.");
+        }
+    }
+}
---
+++ src/test/java/nextstep/courses/domain/ImageSizeTest.java
@@ -0,0 +1,22 @@
+package nextstep.courses.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ImageSizeTest {
+
+    @Test
+    @DisplayName("이미지의 세로 길이가 200pixel 아래일 경우, 예외를 발생시킨다.")
+    void 이미지_세로_체크() {
+        Assertions.assertThatThrownBy(() -> new ImageSize(300, 199))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("이미지의 가로 길이가 300pixel 아래일 경우, 예외를 발생시킨다.")
+    void 이미지_가로_체크() {
+        Assertions.assertThatThrownBy(() -> new ImageSize(299, 200))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
