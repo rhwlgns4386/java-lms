179039adaa2d73d41b0a25d703149e5030c7fc20
hanseounggyun <hanseu9839@gmail.com>
2024-10-25 11:33:27 +0900

    feature: 테스트 성공 코드 구현

--- src/main/java/nextstep/qna/domain/Answer.java
+++ src/main/java/nextstep/qna/domain/Answer.java
@@ -1,12 +1,16 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.qna.NotFoundException;
 import nextstep.qna.UnAuthorizedException;
 import nextstep.users.domain.NsUser;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
 
 public class Answer {
+    private static final String ANSWER_AUTHORITY_ERROR = "다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.";
     private Long id;
 
     private NsUser writer;
@@ -30,11 +34,11 @@ public class Answer {
 
     public Answer(Long id, NsUser writer, Question question, String contents) {
         this.id = id;
-        if(writer == null) {
+        if (writer == null) {
             throw new UnAuthorizedException();
         }
 
-        if(question == null) {
+        if (question == null) {
             throw new NotFoundException();
         }
 
@@ -45,11 +49,6 @@ public class Answer {
 
     public Long getId() {
         return id;
-    }
-
-    public Answer setDeleted(boolean deleted) {
-        this.deleted = deleted;
-        return this;
     }
 
     public boolean isDeleted() {
@@ -64,16 +63,38 @@ public class Answer {
         return writer;
     }
 
-    public String getContents() {
-        return contents;
-    }
-
     public void toQuestion(Question question) {
         this.question = question;
+    }
+
+    public void validateAuthority(NsUser loginUser) throws CannotDeleteException {
+        if (!isOwner(loginUser)) {
+            throw new CannotDeleteException(ANSWER_AUTHORITY_ERROR);
+        }
+    }
+
+    public DeleteHistory deleted(NsUser loginUser) throws CannotDeleteException {
+        validateAuthority(loginUser);
+        deleted = true;
+        return new DeleteHistory(ContentType.ANSWER, id, writer, LocalDateTime.now());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Answer answer = (Answer) o;
+        return Objects.equals(id, answer.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 
     @Override
     public String toString() {
         return "Answer [id=" + getId() + ", writer=" + writer + ", contents=" + contents + "]";
     }
+
 }
--- src/main/java/nextstep/qna/domain/DeleteType.java
+++
@@ -1,2 +0,0 @@
-package nextstep.qna.domain;public enum DeleteType {
-}
