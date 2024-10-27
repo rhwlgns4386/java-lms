6d03f234c29fd8625b811bc2d3b4672c57f7b864
heechul <khc4190@naver.com>
2024-10-27 17:55:25 +0900

    refactor: 메소드 병합

--- src/main/java/nextstep/courses/domain/SessionDate.java
+++ src/main/java/nextstep/courses/domain/SessionDate.java
@@ -7,14 +7,11 @@ public class SessionDate {
     private final LocalDateTime endAt;
 
     public SessionDate(LocalDateTime startAt, LocalDateTime endAt) {
-        validationDate(startAt, endAt);
+        if (endAt.isBefore(startAt)) {
+            throw new IllegalArgumentException("종료일은 시작일보다 작을 수 없습니다.");
+        }
         this.startAt = startAt;
         this.endAt = endAt;
     }
 
-    private static void validationDate(LocalDateTime startAt, LocalDateTime endAt) {
-        if (endAt.isBefore(startAt)) {
-            throw new IllegalArgumentException("종료일은 시작일보다 작을 수 없습니다.");
-        }
-    }
 }
