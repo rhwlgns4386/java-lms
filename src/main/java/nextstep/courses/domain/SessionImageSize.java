f68fe8bf7df2690b1d30327f3c32d1312fa8de19
heechul <khc4190@naver.com>
2024-10-27 17:55:43 +0900

    refactor: 메소드 병합

--- src/main/java/nextstep/courses/domain/SessionImageSize.java
+++ src/main/java/nextstep/courses/domain/SessionImageSize.java
@@ -5,14 +5,11 @@ public class SessionImageSize {
     private final int height;
 
     public SessionImageSize(int width, int height) {
-        validateImageSize(width, height);
+        if (width < 300 || height < 200) {
+            throw new IllegalArgumentException("이미지의 규격이 맞지 않습니다.");
+        }
         this.witdh = width;
         this.height = height;
     }
 
-    private static void validateImageSize(int width, int height) {
-        if (width < 300 || height < 200) {
-            throw new IllegalArgumentException("이미지의 규격이 맞지 않습니다.");
-        }
-    }
 }
