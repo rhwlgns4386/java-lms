d94885fdcc6076468addea53361794b6242d4e82
Hoseop.Kim <kimho314@gmail.com>
2024-10-28 23:22:05 +0900

    [chore] :  Session class에 StudentManager class 연결

--- src/main/java/nextstep/courses/domain/FreeSession.java
+++ src/main/java/nextstep/courses/domain/FreeSession.java
@@ -2,11 +2,12 @@ package nextstep.courses.domain;
 
 public class FreeSession extends Session {
     private SessionStatus sessionStatus;
-
+    private StudentManager studentManager;
 
     public FreeSession(SessionId sessionId, SessionDate sessionDate, Image image) {
         super(sessionId, sessionDate, image, SessionType.FREE);
         this.sessionStatus = SessionStatus.PREPARING;
+        this.studentManager = new StudentManager();
     }
 
     public SessionId getSessionId() {
--- src/main/java/nextstep/courses/domain/PaidSession.java
+++ src/main/java/nextstep/courses/domain/PaidSession.java
@@ -4,12 +4,14 @@ public class PaidSession extends Session {
     private SessionStatus sessionStatus;
     private Integer capacity;
     private Long price;
+    private StudentManager studentManager;
 
     public PaidSession(SessionDate sessionDate, Image image, SessionId sessionId, Integer capacity, Long price) {
         super(sessionId, sessionDate, image, SessionType.PAID);
         this.sessionStatus = SessionStatus.PREPARING;
         this.capacity = capacity;
         this.price = price;
+        this.studentManager = new StudentManager();
     }
 
     public SessionId getSessionId() {
