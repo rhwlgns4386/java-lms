5310e678b33c14e9f363880922e2b79d6070a159
hanseounggyun <hanseu9839@gmail.com>
2024-10-29 15:36:57 +0900

    refactor: ConverImageFileSize 클래스로 fileSize 책임 분리

---
+++ src/main/java/nextstep/courses/domain/session/CoverImageFileSize.java
@@ -0,0 +1,39 @@
+package nextstep.courses.domain.session;
+
+import java.util.Objects;
+
+public class CoverImageFileSize {
+    private static final String OVER_LIMIT_FILE_SIZE_ERROR = "10MB 파일 크기를 초과하였습니다.";
+    private static final int LIMIT_FILE_SIZE = 10 * 1024 * 1024;
+
+    private final int size;
+
+    public CoverImageFileSize(int size) {
+        this.size = isSizeValid(size);
+    }
+
+    public int isSizeValid(int size) {
+        if(size > LIMIT_FILE_SIZE) {
+            throw new IllegalArgumentException(OVER_LIMIT_FILE_SIZE_ERROR);
+        }
+        return size;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        CoverImageFileSize that = (CoverImageFileSize) o;
+        return size == that.size;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(size);
+    }
+
+}
---
+++ src/test/java/nextstep/courses/domain/CoverImageFileSizeTest.java
@@ -0,0 +1,24 @@
+package nextstep.courses.domain;
+
+import nextstep.courses.domain.session.CoverImageFileSize;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class CoverImageFileSizeTest {
+
+    @Test
+    void 이미지_타입_1MB_이하_성공_테스트() {
+        CoverImageFileSize sessionCoverImage = new CoverImageFileSize(1500);
+
+        assertThat(sessionCoverImage).isEqualTo(new CoverImageFileSize(1500));
+    }
+
+    @Test
+    void 이미지_타입_1MB_이하_실패_테스트() {
+        assertThatThrownBy(() -> new CoverImageFileSize(10_500_000))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageMatching("10MB 파일 크기를 초과하였습니다.");
+    }
+
+}
