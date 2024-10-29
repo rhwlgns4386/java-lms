cd88db14e82ff36cae104c8d859298fd8adb219f
minham cho <mhtg90@gmail.com>
2024-10-29 16:41:28 +0900

    test : 강의 유형(SessionType :유료/무료) 기능 테스트 케이스 추가

---
+++ src/test/java/nextstep/courses/domain/SessionTypeTest.java
@@ -0,0 +1,38 @@
+package nextstep.courses.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+public class SessionTypeTest {
+    public static SessionType PAID_SESSION = new SessionType(Long.valueOf(200000), 100);
+    public static SessionType FREE_SESSION = new SessionType();
+    @Test
+    @DisplayName("유료강의_최대인원검증")
+    void check_MaxNumberOfUser() {
+
+        Assertions.assertThatThrownBy(() -> PAID_SESSION.checkMaxNumber(101)).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    @DisplayName("유료강의_결제금액")
+    void check_payAmount() {
+        Assertions.assertThatThrownBy(() -> PAID_SESSION.validatePaymentAmount(Long.valueOf(220000))).isInstanceOf(RuntimeException.class);
+    }
+
+
+    @Test
+    @DisplayName("무료강의_결제금액")
+    void free_session_checkAmount() {
+        Assertions.assertThatCode(() -> FREE_SESSION.validatePaymentAmount(Long.valueOf(0))).doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("무료강의_최대인원검증통과")
+    void free_session_checkNumbersOfPeople() {
+        Assertions.assertThatCode(() -> FREE_SESSION.checkMaxNumber(1000)).doesNotThrowAnyException();
+    }
+
+}
