84a32173353714672c8030596702b9ab274b1e94
minham cho <mhtg90@gmail.com>
2024-10-29 16:46:33 +0900

    test : SessionStatus(강의상태) 기능 테스트 케이스 추가

---
+++ src/test/java/nextstep/courses/domain/SessionStatusTest.java
@@ -0,0 +1,24 @@
+package nextstep.courses.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SessionStatusTest {
+    @ParameterizedTest
+    @ValueSource(strings = {"01","03"})
+    void isValidStatusForApplication_invalidStatus(String testStatus) {
+        SessionStatus sessionStatus = new SessionStatus(testStatus);
+
+        assertThatThrownBy(() -> sessionStatus.isValidStatusForApplication()).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    void isValidStatusForApplication_validStatus() {
+        SessionStatus sessionStatus = new SessionStatus(SessionStatusEnum.RECRUITING.getValue());
+        assertThatCode(()-> sessionStatus.isValidStatusForApplication()).doesNotThrowAnyException();
+    }
+}
