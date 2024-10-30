f7d9914aaad6cf2ac4b444dbca4a10f98dcc63f5
hanseounggyun <hanseu9839@gmail.com>
2024-10-30 12:12:32 +0900

    test: FreeSession 실패 테스트 코드 작성 완료

--- src/test/java/nextstep/courses/domain/session/FreeSessionTest.java
+++ src/test/java/nextstep/courses/domain/session/FreeSessionTest.java
@@ -1,4 +1,18 @@
-package nextstep.courses.domain.image;
+package nextstep.courses.domain.session;
+
+import nextstep.courses.domain.image.SessionCoverImage;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class FreeSessionTest {
+
+    @Test
+    void 무료_강의_저장_테스트() {
+        SessionCoverImage sessionCoverImage = new SessionCoverImage(1L, 150, "leo.pdf", 300, 200);
+        FreeSession freeSession = new FreeSession(sessionCoverImage, CourseStatus.COMPLETED, 100);
+
+
+        Assertions.assertThat(freeSession).isEqualTo(new FreeSession(new SessionCoverImage(1L, 150, "leo.pdf", 300, 200), CourseStatus.COMPLETED, 100L));
+    }
+
 }
