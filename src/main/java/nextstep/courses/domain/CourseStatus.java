589848e42e068581403264fee6619ef4f2c89428
이윤선 <yoonseon3@gmail.com>
2024-10-30 22:41:16 +0900

    feature: Session 도메인 생성

---
+++ src/main/java/nextstep/courses/domain/CourseStatus.java
@@ -0,0 +1,7 @@
+package nextstep.courses.domain;
+
+public enum CourseStatus {
+    PENDING,
+    OPEN,
+    CLOSED
+}
---
+++ src/main/java/nextstep/courses/domain/Session.java
@@ -0,0 +1,35 @@
+package nextstep.courses.domain;
+
+import java.time.LocalDate;
+
+public class Session {
+    private Long id;
+
+    private LocalDate startAt;
+
+    private LocalDate endAt;
+
+    private CoverImage image;
+
+    private PaymentPolicy paymentPolicy;
+
+    private CourseStatus courseStatus;
+
+    private int studentCount;
+
+    public Session(LocalDate startAt, LocalDate endAt, CoverImage image, PaymentPolicy paymentPolicy) {
+        this.startAt = startAt;
+        this.endAt = endAt;
+        this.image = image;
+        this.paymentPolicy = paymentPolicy;
+        this.courseStatus = CourseStatus.PENDING;
+    }
+
+    public static Session create(LocalDate startAt, LocalDate endAt, CoverImage image, PaymentPolicy paymentPolicy) {
+        return new Session(startAt, endAt, image, paymentPolicy);
+    }
+
+    public CourseStatus getCourseStatus() {
+        return courseStatus;
+    }
+}
