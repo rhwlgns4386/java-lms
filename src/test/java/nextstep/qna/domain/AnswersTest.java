bb716b57ad86050b3334d0d9f41cb6876d496825
kimsuyeondev <rhaehl@naver.com>
2024-10-30 23:51:02 +0900

    step1 도메인 객체 테스트 구현

---
+++ src/test/java/nextstep/qna/domain/AnswersTest.java
@@ -0,0 +1,54 @@
+package nextstep.qna.domain;
+
+import nextstep.qna.CannotDeleteException;
+import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class AnswersTest {
+    public static final Answer A1 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+    public static final Answer A2 = new Answer(NsUserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    private List<Answer> sameAnswers = new ArrayList<>();
+    private List<Answer> diffAnswers = new ArrayList<>();
+
+    @BeforeEach
+    void setUp() {
+        sameAnswers.add(A1);
+        sameAnswers.add(A1);
+        diffAnswers.add(A1);
+        diffAnswers.add(A2);
+    }
+
+    @Test
+    @DisplayName("다른 사람이 쓴 답변이 없는 경우 삭제 가능 PASS")
+    void isOwnerCheck() throws CannotDeleteException {
+        Answers answers = new Answers(sameAnswers);
+
+        answers.isOwnerCheck(NsUserTest.JAVAJIGI);
+    }
+
+    @Test
+    @DisplayName("다른 사람이 쓴 답변이 없는 경우 삭제 불가 체크")
+    void isOwnerCheck_CannotDeleteException() {
+        Answers answers = new Answers(diffAnswers);
+
+        assertThatThrownBy(() -> answers.isOwnerCheck(NsUserTest.JAVAJIGI)).isInstanceOf(CannotDeleteException.class);
+    }
+
+    @Test
+    @DisplayName("삭제처리 true 반환")
+    void deleteQuestion(){
+        A1.deleteAnswer();
+        assertThat(A1.isDeleted()).isTrue();
+    }
+
+
+}
--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -1,8 +1,34 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
+import nextstep.users.domain.NsUser;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class QuestionTest {
     public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
     public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+
+    @Test
+    @DisplayName("로그인 사용자와 질문한 사람이 같은 경우 체크로직 삭제 가능 PASS")
+    void isOwnerCheck() throws CannotDeleteException {
+        Q1.isOwnerCheck(NsUserTest.JAVAJIGI);
+    }
+
+    @Test
+    @DisplayName("로그인 사용자와 질문한 사람이 다른 경우 삭제 불가")
+    void isOwnerCheck_CannotDeleteException() {
+        assertThatThrownBy(() -> Q1.isOwnerCheck(NsUserTest.SANJIGI)).isInstanceOf(CannotDeleteException.class);
+    }
+
+    @Test
+    @DisplayName("삭제처리 true 반환")
+    void deleteQuestion() throws CannotDeleteException {
+        Q1.deleteQuestion(NsUserTest.JAVAJIGI);
+        assertThat(Q1.isDeleted()).isTrue();
+    }
 }
