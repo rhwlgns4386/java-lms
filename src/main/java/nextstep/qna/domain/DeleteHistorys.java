c84b4b0893284bac48f58d89770a49a873316a45
kimsuyeondev <rhaehl@naver.com>
2024-10-30 23:50:50 +0900

    step1 리펙토링

---
+++ src/main/java/nextstep/qna/domain/DeleteHistorys.java
@@ -0,0 +1,33 @@
+package nextstep.qna.domain;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DeleteHistorys {
+    private final List<DeleteHistory> deleteHistories = new ArrayList<>();
+
+    public DeleteHistorys(List<DeleteHistory> deleteHistories){
+        this.deleteHistories.addAll(deleteHistories);
+    }
+
+    public List<DeleteHistory> getDeleteHistories() {
+        return deleteHistories;
+    }
+
+    public void addDeleteQuestionHistory(Question question, long questionId) {
+        deleteHistories.add(new DeleteHistory(ContentType.QUESTION, questionId, question.getWriter(), LocalDateTime.now()));
+    }
+
+    public void addDeleteAnswerHistory(List<Answer> answers) {
+        for (Answer answer : answers) {
+            deleteHistories.add(new DeleteHistory(ContentType.ANSWER, answer.getId(), answer.getWriter(), LocalDateTime.now()));
+        }
+    }
+
+    public List<DeleteHistory> addDeleteHistorys(Question question, long questionId, Answers answers) {
+        addDeleteQuestionHistory(question, questionId);
+        addDeleteAnswerHistory(answers.getAnswers());
+        return this.deleteHistories;
+    }
+}
