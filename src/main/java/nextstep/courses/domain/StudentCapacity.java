6cec62e2c90f3d7053cab6847a20f61a30eb9e9f
이윤선 <yoonseon3@gmail.com>
2024-10-30 23:01:32 +0900

    test: 수강신청 인원이 초과할 경우 예외가 발생한다.

--- src/main/java/nextstep/courses/domain/StudentCapacity.java
+++ src/main/java/nextstep/courses/domain/StudentCapacity.java
@@ -3,11 +3,19 @@ package nextstep.courses.domain;
 public class StudentCapacity {
 
     private final int capacity;
+    private int count;
 
     public StudentCapacity(int capacity) {
         if (capacity < 1) {
             throw new IllegalArgumentException("수강 인원은 1명 이상이어야 합니다.");
         }
         this.capacity = capacity;
+    }
+
+    public void increment() {
+        if (count >= capacity) {
+            throw new IllegalArgumentException("정원을 초과했습니다.");
+        }
+        count++;
     }
 }
--- src/test/java/nextstep/courses/domain/StudentCapacityTest.java
+++ src/test/java/nextstep/courses/domain/StudentCapacityTest.java
@@ -3,6 +3,7 @@ package nextstep.courses.domain;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -17,4 +18,13 @@ class StudentCapacityTest {
             .hasMessage("수강 인원은 1명 이상이어야 합니다.");
     }
 
+    @Test
+    @DisplayName("수강신청 인원이 초과할 경우 예외가 발생한다.")
+    void throwExceptionWhenCountThanCapacity() {
+        StudentCapacity studentCapacity = new StudentCapacity(1);
+        studentCapacity.increment();
+        assertThatThrownBy(() -> studentCapacity.increment())
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("정원을 초과했습니다.");
+    }
 }
