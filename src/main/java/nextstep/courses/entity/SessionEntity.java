ded06d17714fceb7b5ae381cfd4308f78fdafed0
heechul <khc4190@naver.com>
2024-11-02 21:19:12 +0900

    feat: 디비가 비어있을 경우 entity에서 예외처리

--- src/main/java/nextstep/courses/entity/SessionEntity.java
+++ src/main/java/nextstep/courses/entity/SessionEntity.java
@@ -1,22 +1,26 @@
 package nextstep.courses.entity;
 
-import nextstep.courses.domain.FreeSession;
-import nextstep.courses.domain.PaidSession;
-import nextstep.courses.domain.Session;
-import nextstep.courses.domain.SessionImage;
+import nextstep.courses.domain.*;
+import org.springframework.util.StringUtils;
 
 import java.sql.Timestamp;
 import java.util.List;
+
+import static nextstep.courses.domain.ProgressStatus.PREPARING;
+import static nextstep.courses.domain.RecruitingStatus.NON_RECRUITING;
 
 public class SessionEntity {
     public static final String FREE_SESSION = "FREE";
     public static final String PAID_SESSION = "PAID";
     public static final int FREE_FEE = 0;
     public static final int FREE_MAX_STUDENTS = 0;
+    public static final String BLANK = "";
     private Long id;
     private Timestamp sessionStartAt;
     private Timestamp sessionEndAt;
     private String status;
+    private String recruitingStatus;
+    private String progressStatus;
     private Long imageId;
     private String sessionType = FREE_SESSION;
     private Integer sessionFee = FREE_FEE;
@@ -26,10 +30,16 @@ public class SessionEntity {
     }
 
     public SessionEntity(Long id, Timestamp sessionStartAt, Timestamp sessionEndAt, String status, String sessionType, Integer sessionFee, Integer maxStudent) {
+        this(id, sessionStartAt, sessionEndAt, status, BLANK, BLANK, sessionType, sessionFee, maxStudent);
+    }
+
+    public SessionEntity(Long id, Timestamp sessionStartAt, Timestamp sessionEndAt, String status, String recruitingStatus, String progressStatus, String sessionType, Integer sessionFee, Integer maxStudent) {
         this.id = id;
         this.sessionStartAt = sessionStartAt;
         this.sessionEndAt = sessionEndAt;
         this.status = status;
+        this.recruitingStatus = recruitingStatus;
+        this.progressStatus = progressStatus;
         this.sessionType = sessionType;
         this.sessionFee = sessionFee;
         this.maxStudent = maxStudent;
@@ -46,16 +56,23 @@ public class SessionEntity {
         entity.id = session.getSessionId();
         entity.sessionStartAt = Timestamp.valueOf(session.getDate().getStartAt());
         entity.sessionEndAt = Timestamp.valueOf(session.getDate().getEndAt());
-        entity.status = session.getRecruitingStatus().name();
+        entity.recruitingStatus = session.getRecruitingStatus().name();
+        entity.progressStatus = session.getProgressStatus().name();
         entity.imageId = session.getImage().getId();
         return entity;
     }
 
     public Session toDomain(SessionImage sessionImage, List<Long> sessionStudents) {
-        if (sessionType.equals(PAID_SESSION)) {
-            return new PaidSession(id, sessionStartAt, sessionEndAt, sessionImage, status, sessionStudents, maxStudent, sessionFee);
+        if (!StringUtils.hasText(recruitingStatus)) {
+            recruitingStatus = NON_RECRUITING.name();
         }
-        return new FreeSession(id, sessionStartAt, sessionEndAt, sessionImage, status, sessionStudents);
+        if (!StringUtils.hasText(progressStatus)) {
+            progressStatus = PREPARING.name();
+        }
+        if (sessionType.equals(PAID_SESSION)) {
+            return new PaidSession(id, sessionStartAt, sessionEndAt, sessionImage, RecruitingStatus.valueOf(recruitingStatus), ProgressStatus.valueOf(progressStatus), sessionStudents, maxStudent, sessionFee);
+        }
+        return new FreeSession(id, sessionStartAt, sessionEndAt, sessionImage, RecruitingStatus.valueOf(recruitingStatus), ProgressStatus.valueOf(progressStatus), sessionStudents);
     }
 
     public Long getId() {
@@ -88,5 +105,13 @@ public class SessionEntity {
 
     public Integer getMaxStudent() {
         return maxStudent;
+    }
+
+    public String getRecruitingStatus() {
+        return recruitingStatus;
+    }
+
+    public String getProgressStatus() {
+        return progressStatus;
     }
 }
