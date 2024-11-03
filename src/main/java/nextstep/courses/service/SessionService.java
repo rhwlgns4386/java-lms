0e51e81ba6d624138294ee7b557fb00157c4f540
kimsuyeondev <rhaehl@naver.com>
2024-11-04 03:34:45 +0900

    step3 - 서비스 구현

--- src/main/java/nextstep/courses/service/SessionService.java
+++ src/main/java/nextstep/courses/service/SessionService.java
@@ -1,6 +1,8 @@
 package nextstep.courses.service;
 
+import nextstep.courses.collection.Students;
 import nextstep.courses.exception.CannotRegisteSessionException;
+import nextstep.courses.infrastructure.SessionRepository;
 import nextstep.courses.request.RequestOrderParam;
 import nextstep.courses.domain.Session;
 import nextstep.courses.factory.SessionFactory;
@@ -9,7 +11,6 @@ import nextstep.courses.domain.SessionInfo;
 import nextstep.courses.domain.SessionType;
 import nextstep.courses.collection.Sessions;
 import nextstep.courses.domain.StateCode;
-import nextstep.courses.infrastructure.SessionRepository;
 import nextstep.payments.domain.Payment;
 import nextstep.users.domain.NsUser;
 
@@ -22,24 +23,26 @@ public class SessionService {
     }
 
     public void registerPaidSession(SessionInfo sessionInfo, SessionImage sessionImage, long salePrice, StateCode stateCode, int studentMaxCount, SessionType sessionType) {
-
         Session session = SessionFactory.createSession(sessionInfo, sessionImage, salePrice, stateCode, studentMaxCount, sessionType);
 
-        sessionRepository.insert(session);
+        sessionRepository.saveRegisterSession(session);
     }
 
-    public void orderPaidSession(Sessions sessions, Payment payment, NsUser user, int idx) throws CannotRegisteSessionException {
-        //db에서 find 가져온다 가정, Sessions 파라미터 삭제 예정
-        Session session = findSessionCreateId(idx, sessions);
+    public Session findSessionInfoById(long idx) {
+        return sessionRepository.findSessionInfoById(idx);
+    }
+
+    public Session orderSession(Payment payment, NsUser user, long idx) throws CannotRegisteSessionException {
+        Session session = findSessionInfoById(idx);
+
+        Students students = sessionRepository.findOrderInfoBySessionId(idx);
+        session.addStudents(students);
 
         RequestOrderParam param = new RequestOrderParam(payment, user);
         session.orderSession(param);
-        //update db student + 1
-    }
 
-    //추후 db로 변경 예정..
-    private Session findSessionCreateId(int idx, Sessions sessions) {
-        return sessions.getSessionIdx(idx);
+        sessionRepository.saveOrderSession(user, session);
+        return session;
     }
 
 }
