a90f5634c29ab9040941c0716aece966caaa3ffe
heechul <khc41@lgensol.com>
2024-11-04 09:52:51 +0900

    refactor: 기존 session_session_image 삭제 및 로직 삭제

--- src/main/java/nextstep/courses/infrastructure/SessionSessionImageRepository.java
+++
@@ -1,8 +0,0 @@
-package nextstep.courses.infrastructure;
-
-import java.util.List;
-
-public interface SessionSessionImageRepository {
-    int save(Long sessionId, List<Long> imageId);
-    List<Long> findBySessionId(Long sessionId);
-}
