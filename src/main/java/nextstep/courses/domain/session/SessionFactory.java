57840fbc9706bc8bdbd68ac07b84c6753e0bae53
green <green@mobidoc.us>
2024-11-06 23:30:05 +0900

    [step4]refactor : 세션 팩토리 적용

--- src/main/java/nextstep/courses/domain/session/SessionFactory.java
+++ src/main/java/nextstep/courses/domain/session/SessionFactory.java
@@ -1,31 +1,7 @@
 package nextstep.courses.domain.session;
 
-import nextstep.courses.domain.cover.CoverImage;
-
-import java.util.List;
-
 public class SessionFactory {
     private SessionFactory() {
-    }
-
-    public static DefaultSession createSession(
-            SessionType sessionType,
-            Long id,
-            SessionProgress progress,
-            SessionRecruitmentStatus recruitment,
-            Period period,
-            List<CoverImage> coverImages,
-            Money courseFee,
-            int maxStudents,
-            List<SessionRegistration> registrations
-    ) {
-        if (SessionType.PAID.getCode().equals(sessionType.getCode())) {
-            return new PaidSession(id, progress, recruitment, period, coverImages, courseFee, maxStudents, registrations);
-        }
-        if (SessionType.FREE.getCode().equals(sessionType.getCode())) {
-            return new FreeSession(id, progress, recruitment, period, coverImages, maxStudents, registrations);
-        }
-        throw new IllegalArgumentException("알 수 없는 세션 타입입니다: " + sessionType);
     }
 
     public static DefaultSession from(DefaultSession session) {
