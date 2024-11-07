c43e0c530a12cbcabf1d8654518668f0b2503861
이윤선 <yoonseon3@gmail.com>
2024-11-07 22:28:58 +0900

    test: 수강신청한 사람 중 승인된 사람이 취소할 수 경우 예외가 발생한다.

--- src/test/java/nextstep/enrollment/domain/EnrollmentTest.java
+++ src/test/java/nextstep/enrollment/domain/EnrollmentTest.java
@@ -2,6 +2,7 @@ package nextstep.enrollment.domain;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -18,5 +19,16 @@ class EnrollmentTest {
         Enrollment enrollment = Enrollment.free(1L, session, NsUserTest.JAVAJIGI);
         enrollment.reject();
         assertDoesNotThrow(() -> enrollment.cancel());
+    }
+
+    @Test
+    @DisplayName("수강신청한 사람 중 승인된 사람이 취소할 수 경우 예외가 발생한다.")
+    void throwExceptionWhen() {
+        Session session = FixtureSessionFactory.createFreeSession(1L);
+        Enrollment enrollment = Enrollment.free(1L, session, NsUserTest.JAVAJIGI);
+        enrollment.approve();
+        Assertions.assertThatThrownBy(() -> enrollment.cancel())
+            .isInstanceOf(IllegalStateException.class)
+            .hasMessage("수강신청 승인되어 취소할 수 없습니다.");
     }
 }
