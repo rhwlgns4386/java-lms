65a63a8957c52d477f381446accc7ac2086312fe
이윤선 <yoonseon3@gmail.com>
2024-11-07 21:11:07 +0900

    chore: 잘못된 description 수정

--- src/main/java/nextstep/session/domain/SessionProgressStatus.java
+++ src/main/java/nextstep/session/domain/SessionProgressStatus.java
@@ -1,8 +1,8 @@
 package nextstep.session.domain;
 
 public enum SessionProgressStatus {
-    PREPARING("진행중"),
-    IN_PROGRESS("모집중"),
+    PREPARING("준비중"),
+    IN_PROGRESS("진행중"),
     COMPLETED("종료");
 
     private final String description;
