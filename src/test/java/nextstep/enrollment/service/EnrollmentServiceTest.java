8e80e40fb5adf87ca06cdde4d55eb9e40c25dec3
이윤선 <yoonseon3@gmail.com>
2024-11-10 15:54:08 +0900

    test: enrollment 메서드가 유료 강의를 수강신청할 때 신청된 수강신청 정보를 반환한다.

--- src/test/java/nextstep/enrollment/service/EnrollmentServiceTest.java
+++ src/test/java/nextstep/enrollment/service/EnrollmentServiceTest.java
@@ -17,11 +17,13 @@ import nextstep.enrollment.domain.ApprovalStatus;
 import nextstep.enrollment.domain.Enrollment;
 import nextstep.enrollment.domain.EnrollmentRepository;
 import nextstep.enrollment.domain.EnrollmentStatus;
+import nextstep.payments.domain.Payment;
 import nextstep.payments.service.PaymentService;
 import nextstep.session.domain.Session;
 import nextstep.session.domain.SessionPolicy;
 import nextstep.session.domain.SessionRepository;
 import nextstep.session.domain.fixture.FixtureEnrollmentFactory;
+import nextstep.session.domain.fixture.FixturePaymentFactory;
 import nextstep.session.domain.fixture.FixtureSessionFactory;
 import nextstep.users.domain.NsUserTest;
 
@@ -65,10 +67,47 @@ class EnrollmentServiceTest {
         then(paymentService).should(never()).getPaymentHistory(NsUserTest.JAVAJIGI, sessionId);
     }
 
+    @Test
+    @DisplayName("enrollment 메서드가 유료 강의를 수강신청할 때 신청된 수강신청 정보를 반환한다.")
+    void enrollmentByPaidSessionTest() {
+        // given
+        Long sessionId = 1L;
+        Session mockSession = getRecruitingPaidSession(sessionId);
+        Enrollment mockEnrollment = getEnrollmentByPaidSession(mockSession);
+        given(sessionRepository.findById(sessionId)).willReturn(Optional.of(mockSession));
+        given(enrollmentRepository.save(any(Enrollment.class))).willReturn(mockEnrollment);
+
+        // when
+        Enrollment enrollment = enrollmentService.enrollment(NsUserTest.JAVAJIGI, sessionId);
+
+        // then
+        assertThat(enrollment.getPayment()).isNotNull();
+        assertThat(enrollment.getId()).isEqualTo(mockEnrollment.getId());
+        assertThat(enrollment.getSessionId()).isEqualTo(mockEnrollment.getSessionId());
+        assertThat(enrollment.getNsUserId()).isEqualTo(mockEnrollment.getNsUserId());
+        assertThat(enrollment.getEnrollmentDate()).isEqualTo(mockEnrollment.getEnrollmentDate());
+        assertThat(enrollment.getApprovalStatus()).isEqualTo(ApprovalStatus.NOT_APPROVED);
+        assertThat(enrollment.getEnrollmentStatus()).isEqualTo(EnrollmentStatus.ACTIVE);
+        then(sessionRepository).should(times(1)).findById(sessionId);
+        then(enrollmentRepository).should(times(1)).save(any(Enrollment.class));
+        then(paymentService).should(times(1)).getPaymentHistory(NsUserTest.JAVAJIGI, sessionId);
+    }
+
     private static Session getRecruitingFreeSession(Long sessionId) {
         Session session = FixtureSessionFactory.createFreeSession(sessionId);
         session.startRecruitment();
         return session;
+    }
+
+    private static Session getRecruitingPaidSession(Long sessionId) {
+        Session session = FixtureSessionFactory.createPaidSession(sessionId, 1L, 5_000L);
+        session.startRecruitment();
+        return session;
+    }
+
+    private static Enrollment getEnrollmentByPaidSession(Session session) {
+        Payment mockPayment = FixturePaymentFactory.create(1L, 1L, 1L, 5_000L);
+        return FixtureEnrollmentFactory.createEnrollmentByPaidSession(session, mockPayment);
     }
 
 }
