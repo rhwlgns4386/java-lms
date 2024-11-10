4efa2703c75df35a2c7fc94873d88714f557f015
이윤선 <yoonseon3@gmail.com>
2024-11-10 16:34:41 +0900

    test: (테스트 실패 중)approve 메서드가 enrollment 유효성 검사 및 enrollment의 approve 메서드를 호출한다.

--- src/test/java/nextstep/enrollment/service/EnrollmentServiceTest.java
+++ src/test/java/nextstep/enrollment/service/EnrollmentServiceTest.java
@@ -3,6 +3,7 @@ package nextstep.enrollment.service;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,6 +23,7 @@ import nextstep.payments.service.PaymentService;
 import nextstep.session.domain.Session;
 import nextstep.session.domain.SessionPolicy;
 import nextstep.session.domain.SessionRepository;
+import nextstep.session.domain.SessionType;
 import nextstep.session.domain.fixture.FixtureEnrollmentFactory;
 import nextstep.session.domain.fixture.FixturePaymentFactory;
 import nextstep.session.domain.fixture.FixtureSessionFactory;
@@ -91,6 +93,37 @@ class EnrollmentServiceTest {
         then(sessionRepository).should(times(1)).findById(sessionId);
         then(enrollmentRepository).should(times(1)).save(any(Enrollment.class));
         then(paymentService).should(times(1)).getPaymentHistory(NsUserTest.JAVAJIGI, sessionId);
+    }
+
+    // TODO : 테스트 실패중. 서비스의 전략 패턴 메서드 호출 검증 방법 점검 필요.
+    @Test
+    @DisplayName("approve 메서드가 enrollment 유효성 검사 및 enrollment의 approve 메서드를 호출한다.")
+    void approveTest() {
+        //given
+        Long enrollmentId = 1L;
+        Enrollment mockEnrollment = mock(Enrollment.class);
+        given(enrollmentRepository.findById(enrollmentId)).willReturn(Optional.of(mockEnrollment));
+        given(mockEnrollment.getSessionType()).willReturn(SessionType.PAID);
+
+        SessionPolicy mockSessionPolicy1 = mock(SessionPolicy.class);
+        SessionPolicy mockSessionPolicy2 = mock(SessionPolicy.class);
+        given(mockSessionPolicy1.isMatch(SessionType.PAID)).willReturn(false);
+        given(mockSessionPolicy2.isMatch(SessionType.PAID)).willReturn(true);
+
+        List<SessionPolicy> mockSessionPolicies = Arrays.asList(
+            mockSessionPolicy1,
+            mockSessionPolicy2
+        );
+        given(sessionPolicies.stream()).willReturn(mockSessionPolicies.stream());
+
+        //when
+        enrollmentService.approve(enrollmentId);
+
+        //then
+        then(enrollmentRepository).should(times(1)).findById(enrollmentId);
+        then(mockEnrollment).should(times(1)).approve();
+        then(mockSessionPolicy1).should(never()).validatePolicy(mockEnrollment);
+        then(mockSessionPolicy2).should(times(1)).validatePolicy(mockEnrollment);
     }
 
     private static Session getRecruitingFreeSession(Long sessionId) {
