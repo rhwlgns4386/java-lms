816280765da43770ef2366aa23d7ff82bec4adf6
hunko <rhwlgns4386@naver.com>
2024-11-30 04:45:28 +0900

    feat : 수강신청 상태 검증 기능 구현

--- src/main/java/nextstep/courses/domain/Enrollments.java
+++ src/main/java/nextstep/courses/domain/Enrollments.java
@@ -6,15 +6,27 @@ import nextstep.courses.DuplicateStudentException;
 import nextstep.users.domain.NsUser;
 
 public class Enrollments {
+    private SessionStatus sessionStatus;
     private final Set<NsUser> enrolledStudents;
 
-    public Enrollments(Set<NsUser> enrolledStudents) {
+    public Enrollments(SessionStatus sessionStatus) {
+        this(sessionStatus, Set.of());
+    }
+
+    public Enrollments(SessionStatus sessionStatus, Set<NsUser> enrolledStudents) {
+        this.sessionStatus = sessionStatus;
         this.enrolledStudents = new HashSet<>(enrolledStudents);
     }
 
     public void enrollment(NsUser student) {
+        validateReayStatus();
         validateDuplicateStudent(student);
+    }
 
+    private void validateReayStatus() {
+        if (!sessionStatus.isEnrolling()) {
+            throw new NonReadyException();
+        }
     }
 
     private void validateDuplicateStudent(NsUser student) {
--- src/test/java/nextstep/courses/domain/EnrollmentsTest.java
+++ src/test/java/nextstep/courses/domain/EnrollmentsTest.java
@@ -6,13 +6,23 @@ import java.util.Set;
 import nextstep.courses.DuplicateStudentException;
 import nextstep.users.domain.NsUserTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class EnrollmentsTest {
 
     @Test
     void 이미추가된_사용자면_예외가_발생한다() {
-        Enrollments enrollments = new Enrollments(Set.of(NsUserTest.JAVAJIGI));
+        Enrollments enrollments = new Enrollments(SessionStatus.ENROLLING, Set.of(NsUserTest.JAVAJIGI));
         assertThatThrownBy(() -> enrollments.enrollment(NsUserTest.JAVAJIGI)).isInstanceOf(
                 DuplicateStudentException.class);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"PREPARING", "CLOSED"})
+    void 진행상태가_아니라면_예외(SessionStatus sessionStatus) {
+        Enrollments enrollments = new Enrollments(sessionStatus);
+        assertThatThrownBy(() -> enrollments.enrollment(NsUserTest.JAVAJIGI)).isInstanceOf(
+                NonReadyException.class);
     }
 }
