b2b5c7a2f653ede986691a2d7eefc802eb6696ee
hunko <rhwlgns4386@naver.com>
2024-12-01 16:48:31 +0900

    refactor : 테스트 함수 제한자 default로 변환

--- src/test/java/nextstep/courses/domain/TestEnrollmentsFactory.java
+++ src/test/java/nextstep/courses/domain/TestEnrollmentsFactory.java
@@ -10,16 +10,16 @@ public class TestEnrollmentsFactory {
     private TestEnrollmentsFactory() {
     }
 
-    public static Enrollments enrollments(SessionStatus sessionStatus, Session session) {
+    static Enrollments enrollments(SessionStatus sessionStatus, Session session) {
         return enrollments(sessionStatus, session, Set.of());
     }
 
-    public static Enrollments enrollments(SessionStatus sessionStatus, Session session, Set<NsUser> enrolledStudents) {
+    static Enrollments enrollments(SessionStatus sessionStatus, Session session, Set<NsUser> enrolledStudents) {
         return new Enrollments(sessionStatus, toStudentSet(session, enrolledStudents));
     }
 
-    public static Enrollments limitEnrollments(int capacity, SessionStatus sessionStatus, Session session,
-                                               Set<NsUser> users) {
+    static Enrollments limitEnrollments(int capacity, SessionStatus sessionStatus, Session session,
+                                        Set<NsUser> users) {
         return new LimitedEnrollments(capacity, sessionStatus, toStudentSet(session, users));
     }
 
