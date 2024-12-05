e6f61c4bdcdce90708508399cc943068b7cf1bb6
hunko <rhwlgns4386@naver.com>
2024-12-05 15:35:26 +0900

    test :EnrollmentStudentTest 테스트 작성

---
+++ src/test/java/nextstep/courses/domain/EnrollmentStudentTest.java
@@ -0,0 +1,23 @@
+package nextstep.courses.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class EnrollmentStudentTest {
+
+    @Test
+    void accept() {
+        EnrollmentStudent enrollmentStudent = new EnrollmentStudent(0, 0, RequestStatus.PENDING);
+        enrollmentStudent.accept();
+
+        assertThat(enrollmentStudent).isEqualTo(new EnrollmentStudent(0, 0, RequestStatus.ACCEPTED));
+    }
+
+    @Test
+    void matchesUserId() {
+        EnrollmentStudent enrollmentStudent = new EnrollmentStudent(1, 0, RequestStatus.PENDING);
+
+        assertThat(enrollmentStudent.matchesUserId(0L)).isTrue();
+    }
+}
